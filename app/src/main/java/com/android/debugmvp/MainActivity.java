a172dba01c93d1e10de4c2a8c1331642ab8b87e4
liudongrong <liudongrong@iandcode.com>
2021-01-05 09:34:42 +0800

    - 完善bind view，借助第三方库

--- app/src/main/java/com/android/debugmvp/MainActivity.java
+++ app/src/main/java/com/android/debugmvp/MainActivity.java
@@ -5,14 +5,26 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 
+import com.android.debugmvp.mvp.MVPActivityImpl;
 import com.android.debugmvp.test.BlogLoopActivity;
+import com.debug.framework.mvp.IBasePresenter;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends MVPActivityImpl {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         startActivity(new Intent(this, BlogLoopActivity.class));
+    }
+
+    @Override
+    protected IBasePresenter createPresenter() {
+        return null;
+    }
+
+    @Override
+    protected void initView() {
+
     }
 }
